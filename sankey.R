ed8c53362d3e99fa5f2bd516bd988f8e896ea269
İmran Kocabıyık <imrankocabiyik@gmail.com>
2016-03-03 15:16:00 +0200

    Create sankey.R

---
+++ sankey.R
@@ -0,0 +1,15 @@
+# installation
+install.packages("RGA")
+library(RGA)
+authorize()
+id <- 000000000 # your accound id here
+first.date <- firstdate(id)
+
+# source report
+sourceReport <- get_ga(id, start.date = "2015-01-01", end.date = "yesterday",
+                  metrics = "ga:sessions",
+                  dimensions = "ga:sourceMedium, ga:pageTitle")
+
+library(googleVis)
+sk1 <- gvisSankey(sourceReport, from="sourceMedium", to="pageTitle", weight="sessions")
+plot(sk1)
